3d8453c83739a3ef2abedcf8d34f46cf8d708fb6
Abhijit Utkarsh <abhijitu18@gmail.com>
2022-11-17 14:02:55 +0530

    make it better

--- main.js
+++ main.js
@@ -122,7 +122,7 @@ app.post("/todo", upload.single("pic"), function (req, res) {
 	}
 
 	saveTodo(todo, function () {
-		res.redirect('https://abhijit-dobby-front.herokuapp.com')
+		res.redirect('http://localhost:3000')
 	})
 	// res.json()
 
